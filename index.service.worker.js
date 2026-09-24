7ea4a8ad65bbfbbfbb19a3e2cf7a90b64522d431
Zorvax-Dev <julien.rgld@gmail.com>
2026-09-24 14:27:56 +0200

    v

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790201974|1823384';
+const CACHE_VERSION = '1790252848|1746784';
 /** @type {string} */
 const CACHE_PREFIX = 'NØMAD ZERO-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
